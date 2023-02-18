8ac1993d09b6d7f50fd9e2188da283cffa902c6e
MarkJulian19 <Ktyf351@mail.ru>
2023-02-18 11:40:12 +0300

    new files

--- prak2_1.cpp
+++ prak2_1.cpp
@@ -1,39 +1,65 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
 
-class ex2
+class complex_num
 {
-    static unsigned int counter;
-    int x;
+    int re, im;
 
 public:
-    ex2()
+    complex_num(const int a, const int b)
     {
-        counter++;
-        // cout << counter << endl;
-        cout << "ex2()" << endl;
+        this->re = a;
+        this->im = b;
     }
-    ~ex2()
+    void add(complex_num a)
     {
-        counter--;
-        // cout << counter << endl;
-        cout << "~ex2()" << endl;
+        this->re += a.re;
+        this->im += a.im;
     }
-    void get_counter(){
-        cout << counter << endl;
-    } 
+    void sub(complex_num a)
+    {
+        this->re -= a.re;
+        this->im -= a.im;
+    }
+    bool compare(complex_num a)
+    {
+        if (a.re == this->re && a.im == this->im)
+            return true;
+        return false;
+    }
+    void multiply(complex_num a)
+    {
+        this->re = this->re * a.re - this->im * a.im;
+        this->im = this->im * a.re + this->re * a.im;
+    }
+    int get_re()
+    {
+        return this->re;
+    }
+    int get_im()
+    {
+        return this->im;
+    }
 };
-
-unsigned int ex2::counter = 0;
 
 int main()
 {
-    ex2 a;
-    a.get_counter();
-    ex2 b;
-    b.get_counter();
-    ex2 c;
-    c.get_counter();
-    return 0;
+    complex_num com1(1, 2), com2(3, 4), com3(5, 6), com4(-1, 0);
+    cout << com1.get_re() << " i" << com1.get_im() << endl;
+    cout << com2.get_re() << " i" << com2.get_im() << endl;
+    cout << com3.get_re() << " i" << com3.get_im() << endl;
+    cout << com4.get_re() << " i" << com4.get_im() << endl << endl;
+
+    com1.add(com2);
+    cout << com1.get_re() << " i" << com1.get_im() << endl << endl;
+
+    com1.sub(com3);
+    cout << com1.get_re() << " i" << com1.get_im() << endl << endl;
+
+    cout << com1.get_re() << " i" << com1.get_im() << endl;
+    cout << com4.get_re() << " i" << com4.get_im() << endl;
+    cout << com1.compare(com4) << endl<< endl;
+    com2.multiply(com3);
+    cout << com2.get_re() << " i" << com2.get_im() << endl;
 }
--- prak2_2.cpp
+++ prak2_2.cpp
@@ -1,65 +1,39 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-class complex_num
+class ex2
 {
-    int re, im;
+    static unsigned int counter;
+    int x;
 
 public:
-    complex_num(const int a, const int b)
+    ex2()
     {
-        this->re = a;
-        this->im = b;
+        counter++;
+        // cout << counter << endl;
+        cout << "ex2()" << endl;
     }
-    void add(complex_num a)
+    ~ex2()
     {
-        this->re += a.re;
-        this->im += a.im;
+        counter--;
+        // cout << counter << endl;
+        cout << "~ex2()" << endl;
     }
-    void sub(complex_num a)
-    {
-        this->re -= a.re;
-        this->im -= a.im;
-    }
-    bool compare(complex_num a)
-    {
-        if (a.re == this->re && a.im == this->im)
-            return true;
-        return false;
-    }
-    void multiply(complex_num a)
-    {
-        this->re = this->re * a.re - this->im * a.im;
-        this->im = this->im * a.re + this->re * a.im;
-    }
-    int get_re()
-    {
-        return this->re;
-    }
-    int get_im()
-    {
-        return this->im;
-    }
+    void get_counter(){
+        cout << counter << endl;
+    } 
 };
+
+unsigned int ex2::counter = 0;
 
 int main()
 {
-    complex_num com1(1, 2), com2(3, 4), com3(5, 6), com4(-1, 0);
-    cout << com1.get_re() << " i" << com1.get_im() << endl;
-    cout << com2.get_re() << " i" << com2.get_im() << endl;
-    cout << com3.get_re() << " i" << com3.get_im() << endl;
-    cout << com4.get_re() << " i" << com4.get_im() << endl << endl;
-
-    com1.add(com2);
-    cout << com1.get_re() << " i" << com1.get_im() << endl << endl;
-
-    com1.sub(com3);
-    cout << com1.get_re() << " i" << com1.get_im() << endl << endl;
-
-    cout << com1.get_re() << " i" << com1.get_im() << endl;
-    cout << com4.get_re() << " i" << com4.get_im() << endl;
-    cout << com1.compare(com4) << endl<< endl;
-    com2.multiply(com3);
-    cout << com2.get_re() << " i" << com2.get_im() << endl;
+    ex2 a;
+    a.get_counter();
+    ex2 b;
+    b.get_counter();
+    ex2 c;
+    c.get_counter();
+    return 0;
 }
