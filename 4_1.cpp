33b2f87b8553acb6f5fb3860f7ad29e8697e05a5
MarkJulian19 <ktyf351@mail.ru>
2023-04-07 20:58:52 +0300

    4_1.cpp

--- 4_1.cpp
+++ 4_1.cpp
@@ -1,66 +1,74 @@
-// ДЗ-4. Шаблоны. Срок сдачи — до 01.04.23
-
 // Написать класс-функцию (с перегруженным operator()), для выполнения некоторых действий над передаваемым ей контейнером.
 // Действия можете придумать любые (например: поменять местами элементы, реверсировать и т. п.), напишите комментарий, что делаете.
-
 // Функция должна быть применима к list, vector, string, хорошо, если и к массивам встроенных типов.
-
 // Количество параметров в функции - по необходимости.
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <list>
 #include <string>
 #include <iterator>
 using namespace std;
-class Exchange {  //Меняет местами элемент p1 и p2
+
+class Rev // Переворачивает массив
+{
 public:
-	template <typename CustomType> //Для контейнера
-	void operator () (CustomType &obj, int p1, int p2) {
-		typename CustomType::iterator i = obj.begin(), j = obj.begin(), tmp;
-        for (int k=0; k<p1; k++){
-            i++;
-        }
-        for (int k=0; k<p2; k++){
-            j++;
-        }
-        swap(*i,*j);
-	}
-	template <typename CustomType> //Для массива
-	void operator () (CustomType *arr, int p1, int p2) {
-		swap(arr[p1],arr[p2]);
-	}
+    template <typename Container> 
+    void operator()(Container &obj)
+    {
+        reverse(obj.begin(), obj.end());
+    }
+
+    template <typename Container> 
+    void operator()(Container *arr, int n)
+    {
+        for (int i = 0, j = n-1; i < j; i++, j--)
+            swap(arr[i], arr[j]);
+    }
 };
-template <typename CustomType>
-void print(const CustomType & obj) {
-	typename CustomType::const_iterator i;
-	for (i = obj.begin(); i != obj.end(); i++)
-		cout << *i << ' ';
-	cout<<endl;
+
+template <typename Container>
+void print(const Container &obj)
+{
+    typename Container::const_iterator i;
+    for (i = obj.begin(); i != obj.end(); i++)
+        cout << *i << ' ';
+    cout << endl;
 }
-int main(){
-    Exchange f;
+
+int main()
+{
+    Rev fun;
+    vector<int> V{1, 2, 3, 4, 5};
+    cout << "vector" << endl;
+    print(V);
+    fun(V);
+    print(V);
+
+    int M[] = {1, 2, 3, 4, 5};
+    cout << "mass" << endl;
+    for (int i = 0; i < (sizeof(M) / sizeof(int)); i++)
+    {
+        cout << M[i] << " ";
+    }
+    cout << endl;
+    fun(M, sizeof(M) / sizeof(int));
+    for (int i = 0; i < (sizeof(M) / sizeof(int)); i++)
+    {
+        cout << M[i] << " ";
+    }
+    cout << endl;
+
+    cout << "list" << endl;
     list<int> L;
     L={1,2,3,4,5};
     print(L);
-    f(L, 0, 2);
+    fun(L);
     print(L);
-    vector<int> v1={1,2,3,4,5};
-    print(v1);
-    f(v1, 0, 2);
-    print(v1);
-    string s1="abcdef";
-    print(s1);;
-    f(s1, 0, 2);
-    print(s1);
-    int s2[5]{1,2,3,4,5};
-    for (int i=0; i<5; i++){
-        cout<<s2[i]<<' ';
-    }
-    cout<<endl;
-    f(s2,0, 2);
-    for (int i=0; i<5; i++){
-        cout<<s2[i]<<' ';
-    }
-    cout<<endl;
-    f(s1,0, 2);
+
+    cout << "string" << endl;
+    string s="1245";
+    print(s);
+    fun(s);
+    print(s);
 }
